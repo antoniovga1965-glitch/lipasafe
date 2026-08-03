bfe9e808ae11c6b2c8dfedcf0951e00d08bf59fe
Smartkid TGA <smartkid@vezoro.dev>
2026-08-03 23:49:25 +0300

    fix: add REDIS_PASSWORD to redis client auth

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -5,6 +5,7 @@ const Redis = require('ioredis')
 const redis = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
   retryStrategy: (times) => Math.min(times * 50, 2000),
 })
@@ -12,7 +13,7 @@ const redis = new Redis({
 redis.on('error', (err) => console.error('Redis error:', err))
 redis.on('connect', () => console.log('Redis connected'))
 
-const redisConnection = { host: process.env.REDIS_HOST || '127.0.0.1', port: process.env.REDIS_PORT || 6379 }
+const redisConnection = { host: process.env.REDIS_HOST || '127.0.0.1', port: process.env.REDIS_PORT || 6379, password: process.env.REDIS_PASSWORD }
 module.exports = redis
 module.exports.redisConnection = redisConnection
 
