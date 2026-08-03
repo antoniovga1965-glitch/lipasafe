d4883bda444c556dc4f3f31b78b285093dfea8f8
Smartkid TGA <smartkid@vezoro.dev>
2026-08-03 20:31:42 +0300

    fix: correct prisma seed import path for deployment

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,6 @@
 'use strict'
 require('dotenv').config()
-const prisma = require('../../api-gateway/src/utils/prisma')
+const prisma = require('../src/utils/prisma')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
 
